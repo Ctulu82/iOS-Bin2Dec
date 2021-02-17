3c057291af29ef258202e80e5fb3dde89113750c
TaeHyun Kim <taehyun.kim1982@gmail.com>
2021-02-17 17:09:38 +0900

    [TEST] logging Transformer functions.

--- iOS-Bin2Dec/ViewController.swift
+++ iOS-Bin2Dec/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let transformer = Transformer()
+
+        print(transformer.decToBin(30))
+
+        print(transformer.binToDec("11110")!)
+
+        let binary = "11001"
+        let number = strtoul(binary, nil, 2)
+        print(number) // Output: 25
     }
 
 
